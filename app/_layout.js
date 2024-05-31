449097e2bc86c647757a8df0c09b83e597447a5f
AnkahHarrison <ankahharrison@gmail.com>
2024-05-31 00:10:40 +0000

    chore:(plugins)added the nativewind/babel

--- app/_layout.js
+++ app/_layout.js
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 const _layout = () => {
   return (
-    <View>
-      <Text>_layout</Text>
+    <View className="flex-1 justify-center items-center text-2xl ">
+      <Text className="text-4xl bg-red-600 text-center">_layout</Text>
     </View>
   );
 };

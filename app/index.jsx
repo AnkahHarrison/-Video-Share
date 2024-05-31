919603f264cab023159164bb9e8d3519e689fab5
AnkahHarrison <ankahharrison@gmail.com>
2024-05-31 03:06:00 +0000

    feat: added assets

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+const RootLayout = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-red-400">index</Text>
+
+      <Link className="text-2xl text-green-500" href="/#">
+        Go to profile
+      </Link>
+    </View>
+  );
+};
+
+export default RootLayout;
